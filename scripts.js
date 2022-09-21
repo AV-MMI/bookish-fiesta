e0759152422acaaf501b7bebd6c9888ff8cf26cc
AV-MMI <villalobos.aaron@hotmail.com>
2022-09-20 20:25:53 -0400

    finished the toggle button interaction, and the remove book button interation. Added updateInfoObj() along with displayInfoObj(). Working on the modal window that pops up when the new book button is clicked

--- scripts.js
+++ scripts.js
@@ -1,11 +1,42 @@
-const myLibrary = [];
+let myLibrary = [{
+	title: 'leto',
+	author: 'a',
+	pages: 10,
+	read: true,
+},{
+	title: 'lto',
+	author: 'b',
+	pages: 10,
+	read: false,
+},{
+	title: 'lo',
+	author: 'c',
+	pages: 10,
+	read: false,
+}];
+
+let infoObj = {
+	created: 0,
+	removed: 0,
+	toRead: 0,
+	haveRead: 0,
+}
 
 // DOM elements
-const toReadDisplay = document.getElementById('book-container-0')
+const librarySection = document.getElementById('library-section');
+const newBookBtn = document.getElementById('new-book-btn');
+const toReadDisplay = document.getElementById('book-container-0');
 const haveReadDisplay = document.getElementById('book-container-1');
 
+const infoCreated = document.getElementById('created-val');
+const infoRemoved = document.getElementById('removed-val');
+const infoToRead = document.getElementById('toRead-val');
+const infoHaveRead = document.getElementById('haveRead-val');
+
+newBookBtn.addEventListener('click', displayModalWindow);
+
 // constructor
-function Book(title, author, pages, haveRead){
+function Book(title, author, pages, read){
 	if(!this instanceof Book){
 		return;
 	} else {
@@ -24,25 +55,17 @@ function addBookToLibrary(library, bookObj){
 	library.push(bookObj);
 }
 
-let leto = new Book("a", "b", 10, true);
-let lto = new Book("ab", "cb", 10, true);
-let lo = new Book("aas", "cb", 10, true);
-
-addBookToLibrary(myLibrary, leto);
-addBookToLibrary(myLibrary, lto);
-addBookToLibrary(myLibrary, lo);
-
 function displayBooks(bookObj){
 	if(bookObj.length){
 		for(let i = 0; i < bookObj.length; i++){
-			if(bookObj[i].haveRead){
+			if(bookObj[i].read){
 				haveReadDisplay.appendChild( DOMCreateBook(bookObj[i]) );
 			} else {
 				toReadDisplay.appendChild( DOMCreateBook(bookObj[i]) );
 			}
 		}
 	} else {
-		if(bookObj.haveRead){
+		if(bookObj.read){
 			haveReadDisplay.appendChild( DOMCreateBook(bookObj) );
 		} else {
 			toReadDisplay.appendChild( DOMCreateBook(bookObj) );
@@ -50,9 +73,12 @@ function displayBooks(bookObj){
 	}
 }
 
+displayBooks(myLibrary);
+
 function DOMCreateBook(obj){
 	let bookItemContainer = document.createElement('div');
-
+	bookItemContainer.setAttribute('title', obj.title);
+	bookItemContainer.setAttribute('author', obj.author);
 	//bookItem
 	let book = document.createElement('div');
 	let toggleBtn = document.createElement('button');
@@ -64,17 +90,18 @@ function DOMCreateBook(obj){
 	toggleBtn.classList.add('btn', 'toggle-btn');
 	toggleBtn.setAttribute('type', 'button');
 	toggleBtn.addEventListener('click', toggleClass);
-	toggleBtn.addEventListener('transitionend', toggleTransition);
+	toggleBtn.addEventListener('transitionend', toggleDisplayContainer);
 	circle.classList.add('circle');
 
-	if(obj.haveRead){
+	if(obj.read){
 		toggleBtn.classList.add('toogle-btn-active');
 		circle.classList.add('circle-active');
 	}
 
+	removeBtn.textContent = "X";
 	removeBtn.classList.add('btn', 'remove-btn');
 	removeBtn.setAttribute('type', 'button');
-	removeBtn.textContent = "X";
+	removeBtn.addEventListener('click', removeBook);
 	titleSpan.textContent = obj['title'];
 
 	toggleBtn.appendChild(circle);
@@ -173,13 +200,12 @@ function DOMCreateBook(obj){
 	bookItemContainer.appendChild(editFormContainer);
 	bookItemContainer.appendChild(editBtnContainer);
 
+	updateInfoProp('created');
 	return bookItemContainer;
 }
 
-displayBooks(leto)
-
 	/*
-	* EVENT LISTENER
+	* EVENT HANDLER
 	*/
 function toggleClass(event){
 	if(event.target.classList.contains('btn')){
@@ -192,13 +218,183 @@ function toggleClass(event){
 }
 
 	/*
-	* EVENT LISTENER
+	* EVENT HANDLER
+	*/
+function toggleDisplayContainer(event){
+	let container;
+	let objTitle;
+	let objAuthor;
+	if(event.target.parentElement.classList.contains('book')){
+		container = event.target.parentElement.parentElement.parentElement;
+		objTitle = event.target.parentElement.parentElement.getAttribute('title');
+		objAuthor = event.target.parentElement.parentElement.getAttribute('author');
+	}
+
+	//Invert the value of haveRead property.
+	for(let i = 0; i < myLibrary.length; i++){
+		if(myLibrary[i].title == objTitle && myLibrary[i].author == objAuthor){
+			myLibrary[i].read = (myLibrary[i].read) ? false : true;
+		}
+	}
+
+	//remove book item.
+	removeBookNode(container, objTitle);
+	//invoke bookDisplay
+	displayBooks( findObj(myLibrary, objTitle) );
+}
+
+	/*
+	* FUNCTION
+	*/
+function findObj(arr, title){
+	for(let i = 0; i < arr.length; i++){
+		if(arr[i].title == title){
+			return arr[i];
+		}
+	}
+}
+	/*
+	* FUNCTION
+	*/
+function updateInfoProp(prop){
+	infoObj[prop]++;
+	displayInfo(infoObj);
+}
+
+	/*
+	* FUNCTION
+	*/
+function displayInfo(obj){
+	infoCreated.textContent = obj.created;
+	infoRemoved.textContent = obj.removed;
+	infoToRead.textContent = obj.toRead;
+	infoHaveRead.textContent = obj.haveRead;
+}
+
+	/*
+	* EVENT HANDLER
+	*/
+function removeBook(event){
+	let container = event.target.parentElement.parentElement.parentElement;
+	let objTitle = event.target.parentElement.parentElement.title;
+	let objAuthor = event.target.parentElement.parentElement.getAttribute('author');
+
+	myLibrary = removeFromArr(myLibrary, objTitle, objAuthor);
+	removeBookNode(container, objTitle);
+	updateInfoProp('removed');
+}
+
+	/*
+	* AUXILIAR FUNCTIONS OF THE REMOVE BTN EVENT HANLDER.
 	*/
 
-	//TODO: finish function. Must change the display container of the item every time
-	// 		that its haveRead state changes.
-function toggleTransition(event){
-	if(event.target.parentElement.classList.contains('book')){
-		console.log(event.target.parentElement.children[1].textContent);
+//remove bookFrom from array
+function removeFromArr(array, objTitle, objAuthor){
+	console.log(objTitle, objAuthor);
+	let target;
+	let newArr;
+	for(let i = 0; i < array.length; i++){
+		if(array[i].title == objTitle && array[i].author == objAuthor){
+			target = i;
+			console.log(target)
+		}
+	}
+
+	if(target == 0){
+		newArr = array.slice(1, array.length);
+	}
+
+	else if(target == (array.length - 1)){
+		newArr = array.slice(0, array.length-1);
+	}
+
+	else {
+		newArr = array.slice(0, target).concat( array.slice(target+1, array.length) );
+	}
+
+	return newArr;
+}
+
+// remove target book from the DOM
+function removeBookNode(container, objTitle){
+	for(let i = 0; i < container.children.length; i++){
+		if(container.children[i].getAttribute('title') == objTitle){
+			container.children[i].remove();
+		}
+	}
+}
+
+function displayModalWindow(event){
+	let modalWindow = document.createElement('div');
+	modalWindow.classList.add('modal-bg');
+
+	let contentContainer = document.createElement('div');
+	contentContainer.classList.add('.modal-newBook-container');
+
+	let closeContainer = document.createElement('div');
+	closeContainer.setAttribute('id', 'close-container');
+
+	let closeBtn = document.createElement('button');
+	closeBtn.setAttribute('type', 'button');
+	closeBtn.classList.add('close-btn');
+	closeBtn.addEventListener('click', closeModalWindow);
+	closeBtn.textContent = "X";
+
+	let form = document.createElement('form');
+	let ul = document.createElement('ul');
+	let liTitle = document.createElement('li');
+	let titleLabel = document.createElement('label');
+	let titleInput = document.createElement('input');
+
+	let liAuthor = document.createElement('li');
+	let authorLabel = document.createElement('label');
+	authorLabel.textContent = 'Author:';
+	let authorInput = document.createElement('input');
+
+	let liPages = document.createElement('li');
+	let pagesLabel = document.createElement('label');
+	pagesLabel.textContent = 'Pages:';
+	let pagesInput = document.createElement('input');
+
+	let liHaveRead = document.createElement('li');
+	let haveReadSpan = document.createElemenet('span');
+	haveReadSpan.textContent = "Have you read this book?"
+	let ulHaveRead = document.createElement('ul');
+	let liTrue = document.createElement('li');
+	let trueLabel = document.createElement('label');
+	let trueInput = document.createElement('input');
+	let liFalse = document.createElement('li');
+	let falseLabel = document.createElement('label');
+	let falseInput = document.createElement('input');
+
+	ulHaveRead.appendChild(liTrue);
+	ulHaveRead.appendChild(liFalse);
+
+	liHaveRead.appendChild(haveReadSpan);
+	liHaveRead.appendChild(ulHaveRead);
+
+	ul.appendChild(liTitle);
+	ul.appendChild(liAuthor);
+	ul.appendChild(liPages);
+	ul.appendChild(liHaveRead);
+
+	form.appendChild(ul);
+	closeContainer.appendChild(closeBtn);
+	contentContainer.appendChild(closeContainer);
+	modalWindow.appendChild(contentContainer);
+
+	librarySection.appendChild(modalWindow);
+}
+
+/*
+* EVENT HANDLER
+*/
+
+function closeModalWindow(event){
+	let modalWindow = event.target.parentElement.parentElement.parentElement;
+	modalWindow.classList.remove('modal-bg');
+
+	for(let i = 0; i < modalWindow.children.length; i++){
+		modalWindow.children[i].remove();
 	}
 }
